e9f07d3b40a883c0e067a6d2bb026a3dfb4371a7
Rich Elton <richelton14@gmail.com>
2020-03-13 15:46:21 -0300

    Thinking about new features

--- src/components/ServiceItem/index.js
+++ src/components/ServiceItem/index.js
@@ -6,14 +6,26 @@ export default function ServiceItem({ item }) {
   return (
     <div className="div-item">
       <div className="info-author">
-        <div className="img-author">
-          <img src={item.url_foto} alt="Avatar do usuário" />
+        <div className="div-author-info">
+          <div className="img-author">
+            <img src={item.url_foto} alt="Avatar do usuário" />
+          </div>
+
+          <div className="author">
+            <strong id="name-author">{item.name}</strong>
+            <strong id="occupation-author">{item.occupation}</strong>
+            <span>Avaliações: {item.rating}</span>
+            <span>Classificação: {item.ranking}</span>
+          </div>
         </div>
 
-        <div className="author">
-          <strong id="name-author">{item.name}</strong>
-          <strong id="occupation-author">{item.occupation}</strong>
+        <div>
+          <strong id="occupation-author">{item.price}</strong>
         </div>
+      </div>
+
+      <div>
+        <button>Ver perfil</button>
       </div>
     </div>
   );
